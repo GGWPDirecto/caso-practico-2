940203dffd2bd1fa42a78ac149891d7efe149f9e
Carlos Hernandez <carloshern@outlook.es>
2025-07-19 12:55:51 +0200

    vudelta

--- terraform/network.tf
+++ terraform/network.tf
@@ -6,11 +6,20 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+# Subred para la VM
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-casopractico2"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Subred para AKS
+resource "azurerm_subnet" "subnet_aks" {
+  name                 = "subnet-aks"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_security_group" "nsg" {
