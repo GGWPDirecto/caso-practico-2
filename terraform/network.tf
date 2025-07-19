f623bd9c543d34ea4a40943a87ee8dfca051f2c3
Carlos Hernandez <carloshern@outlook.es>
2025-07-19 21:55:38 +0200

    lll

--- terraform/network.tf
+++ terraform/network.tf
@@ -54,6 +54,19 @@ resource "azurerm_network_security_group" "nsg_vm" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  # Regla para permitir el tráfico HTTPS entrante
+  security_rule {
+    name                       = "AllowHTTPS"
+    priority                   = 111
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # IP pública para la VM
